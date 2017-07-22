b5dda7d5cca1a8875fab3dc728a33773fcef16ce
Daniel Perez <dperez@localhost.localdomain>
2017-07-22 12:54:08 -0500

    first version

---
+++ app/modules/rul/js/rul.controller.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('rul.module')
+        .controller('rul.controller', rul);
+
+    rul.$inject = [];
+
+    function rul() {
+        var vm = this;
+        
+        vm.test = "hola";
+    }
+})();
---
+++ app/rulNumbers.js
@@ -0,0 +1,305 @@
+var numbers = [
+	{
+		id: -1,
+		color: 'green',
+		zone: 'doubleCero',
+		right: 27,
+		left: 1,
+		typeNumber: 'cero'
+	},
+	{
+		id: 0,
+		color: 'green',
+		right: 28,
+		left: 2,
+		typeNumber: 'cero'
+	},
+	{
+		id: 1,
+		color: 'red',
+		zone: 'doubleCero',
+		right: -1,
+		left: 13,
+		typeNumber: 'odd'
+	},
+	{
+		id: 2,
+		color: 'black',
+		zone: 'cero',
+		right: 0,
+		left: 14,
+		typeNumber: 'even'
+	},
+	{
+		id: 3,
+		color: 'red',
+		zone: 'doubleCero',
+		right: 24,
+		left: 15,
+		typeNumber: 'odd'
+	},
+	{
+		id: 4,
+		color: 'black',
+		zone: 'cero',
+		right: 23,
+		left: 16,
+		typeNumber: 'even'
+	},
+	{
+		id: 5,
+		color: 'red',
+		zone: 'orphan',
+		right: 22,
+		left: 17,
+		typeNumber: 'odd'
+	},
+	{
+		id: 6,
+		color: 'black',
+		zone: 'orphan',
+		right: 21,
+		left: 18,
+		typeNumber: 'even'
+	},
+	{
+		id: 7,
+		color: 'red',
+		zone: 'cero',
+		right: 11,
+		left: 20,
+		typeNumber: 'odd'
+	},
+	{
+		id: 8,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 19,
+		left: 12,
+		typeNumber: 'even'
+	},
+	{
+		id: 9,
+		color: 'red',
+		zone: 'cero',
+		right: 26,
+		left: 28,
+		typeNumber: 'odd'
+	},
+	{
+		id: 10,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 25,
+		left: 27,
+		typeNumber: 'even'
+	},
+	{
+		id: 11,
+		color: 'black',
+		zone: 'cero',
+		right: 7,
+		left: 30,
+		typeNumber: 'odd'
+	},
+	{
+		id: 12,
+		color: 'red',
+		zone: 'doubleCero',
+		right: 8,
+		left: 29,
+		typeNumber: 'even'
+	},
+	{
+		id: 13,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 1,
+		left: 36,
+		typeNumber: 'odd'
+	}, {
+		id: 14,
+		color: 'red',
+		zone: 'cero',
+		right: 2,
+		left: 35,
+		typeNumber: 'even'
+	},
+	{
+		id: 15,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 3,
+		left: 34,
+		typeNumber: 'odd'
+	},
+	{
+		id: 16,
+		color: 'red',
+		zone: 'cero',
+		right: 4,
+		left: 33,
+		typeNumber: 'even'
+	},
+	{
+		id: 17,
+		color: 'black',
+		zone: 'orphan',
+		right: 5,
+		left: 32,
+		typeNumber: 'odd'
+	},
+	{
+		id: 18,
+		color: 'red',
+		zone: 'orphan',
+		right: 6,
+		left: 31,
+		typeNumber: 'even'
+	},
+	{
+		id: 19,
+		color: 'red',
+		zone: 'orphan',
+		right: 31,
+		left: 8,
+		typeNumber: 'odd'
+	},
+	{
+		id: 20,
+		color: 'black',
+		zone: 'orphan',
+		right: 32,
+		left: 7,
+		typeNumber: 'even'
+	},
+	{
+		id: 21,
+		color: 'red',
+		zone: 'orphan',
+		right: 33,
+		left: 6,
+		typeNumber: 'odd'
+	},
+	{
+		id: 22,
+		color: 'black',
+		zone: 'orphan',
+		right: 34,
+		left: 5,
+		typeNumber: 'even'
+	},
+	{
+		id: 23,
+		color: 'red',
+		zone: 'cero',
+		right: 35,
+		left: 4,
+		typeNumber: 'odd'
+	},
+	{
+		id: 24,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 36,
+		left: 3,
+		typeNumber: 'even'
+	},
+	{
+		id: 25,
+		color: 'red',
+		zone: 'doubleCero',
+		right: 29,
+		left: 10,
+		typeNumber: 'odd'
+	},
+	{
+		id: 26,
+		color: 'black',
+		zone: 'cero',
+		right: 30,
+		left: 9,
+		typeNumber: 'even'
+	},
+	{
+		id: 27,
+		color: 'red',
+		zone: 'doubleCero',
+		right: 10,
+		left: -1,
+		typeNumber: 'odd'
+	},
+	{
+		id: 28,
+		color: 'black',
+		zone: 'cero',
+		right: 9,
+		left: 0,
+		typeNumber: 'even'
+	},
+	{
+		id: 29,
+		color: 'black',
+		zone: 'doubleCero',
+		right: 12,
+		left: 25,
+		typeNumber: 'odd'
+	},
+	{
+		id: 30,
+		color: 'red',
+		zone: 'cero',
+		right: 26,
+		left: 11,
+		typeNumber: 'even'
+	},
+	{
+		id: 31,
+		color: 'black',
+		zone: 'orphan',
+		right: 18,
+		left: 19,
+		typeNumber: 'odd'
+	},
+	{
+		id: 32,
+		color: 'red',
+		zone: 'orphan',
+		right: 17,
+		left: 20,
+		typeNumber: 'even'
+	},
+	{
+		id: 33,
+		color: 'black',
+		zone: 'orphan',
+		right: 16,
+		left: 21,
+		typeNumber: 'odd'
+	},
+	{
+		id: 34,
+		color: 'red',
+		zone: 'orphan',
+		right: 15,
+		left: 22,
+		typeNumber: 'even'
+	},
+	{
+		id: 35,
+		color: 'black',
+		zone: 'cero',
+		right: 14,
+		left: 23,
+		typeNumber: 'odd'
+	},
+	{
+		id: 36,
+		color: 'red',
+		zone: 'doubleCero',
+		right: 13,
+		left: 24,
+		typeNumber: 'even'
+	}
+
+];
